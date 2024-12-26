81723b46249efefd238bbb9ac9bb465cab3059ef
Abdurrahman Abubakar <abdul1012mubarak@gmail.ccom>
2024-12-26 17:29:12 +0100

    ,mppokpokop

--- src/components/movies.jsx
+++ src/components/movies.jsx
@@ -17,7 +17,7 @@ class Movies extends Component {
   };
 
   componentDidMount() {
-    const genres = [{ _id: "", name: "All Genres 1" }, ...getGenres()];
+    const genres = [{ _id: "", name: "All Genres 12" }, ...getGenres()];
 
     this.setState({ movies: getMovies(), genres });
   }
